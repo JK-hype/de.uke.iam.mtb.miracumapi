ccd7aa52bc9c0a2f21d998f60bee8611fb37b5e8
Jan Kramer <ja.kramer@uke.de>
2022-09-16 17:12:41 +0000

    Refactoring

---
+++ src/main/java/de/uke/iam/mtb/miracumapi/dao/MiracumPathsRepository.java
@@ -0,0 +1,9 @@
+package de.uke.iam.mtb.miracumapi.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.uke.iam.mtb.miracumapi.model.MiracumPathsEntity;
+
+public interface MiracumPathsRepository extends JpaRepository<MiracumPathsEntity, String> {
+
+}
---
+++ src/main/java/de/uke/iam/mtb/miracumapi/model/MiracumPathsEntity.java
@@ -0,0 +1,21 @@
+package de.uke.iam.mtb.miracumapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(schema = "miracum", name = "paths")
+@Data
+public class MiracumPathsEntity {
+
+    @Id
+    private String patientId;
+    private String pathToOutput;
+    private String pathToMaf;
+    private String pathToReport;
+    private String pathToInput;
+    private String pathToLogs;
+}
